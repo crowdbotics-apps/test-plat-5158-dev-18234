f8b960488e5c76ebe2ee9ff0566f5d641f796d04
Crowdbotics <team@crowdbotics.com>
2021-01-13 12:12:17 +0000

    Update components

--- src/features/BlankScreen016970/screens/blank.js
+++ src/features/BlankScreen016970/screens/blank.js
@@ -21,7 +21,7 @@ export class Blank extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = { DateTimePicker_4: new Date("") }
   }
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
@@ -33,16 +33,31 @@ export class Blank extends React.Component {
         style={styles.Button_2}
         onPress={() => alert("Pressed!")}
       />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_4}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_4: selectedDate })
+        }
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({ View_1: {}, Button_2: { height: 55 } })
+const styles = StyleSheet.create({
+  View_1: {},
+  Button_2: { height: 55 },
+  Button_3: {},
+  DateTimePicker_4: {}
+})
 
 const mapStateToProps = state => {
   return {}
 }
+
 const mapDispatchToProps = () => {
   return {}
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(Blank)
