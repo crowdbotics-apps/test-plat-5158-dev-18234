9cef674becf47a3869762a1c14b8942f0458c950
Crowdbotics <team@crowdbotics.com>
2021-01-13 12:11:54 +0000

    Update components

---
+++ src/features/BlankScreen016970/screens/blank.js
@@ -0,0 +1,48 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  render = () => (
+    <View>
+      <Button
+        title="Press me!"
+        style={styles.Button_2}
+        onPress={() => alert("Pressed!")}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({ View_1: {}, Button_2: { height: 55 } })
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
